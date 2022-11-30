7fca0f8ef0632039d203af7043a6795ec9e08e5e
bill.wallis <bill.wallis@allica.bank>
2022-11-30 08:01:01 +0000

    Initial commit

---
+++ Creational-Patterns/Singleton/example.py
@@ -0,0 +1,50 @@
+"""
+https://refactoring.guru/design-patterns/singleton
+https://refactoring.guru/design-patterns/singleton/python/example
+https://python-patterns.guide/gang-of-four/singleton/
+
+Note: the singleton pattern is rarely implemented in Python applications as Python offers the `__new__()` dunder
+Additionally, a case that requires a singleton would be better designed to use the Global Object pattern instead
+"""
+
+
+class SingletonMeta(type):
+    """
+    The Singleton class can be implemented in different ways in Python. Some
+    possible methods include: base class, decorator, metaclass. We will use the
+    metaclass because it is best suited for this purpose.
+    """
+
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        """
+        Possible changes to the value of the `__init__` argument do not affect
+        the returned instance.
+        """
+        if cls not in cls._instances:
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
+
+
+class Singleton(metaclass=SingletonMeta):
+    def some_business_logic(self):
+        """
+        Finally, any singleton should define some business logic, which can be
+        executed on its instance.
+        """
+
+        # ...
+
+
+if __name__ == "__main__":
+    # The client code.
+
+    s1 = Singleton()
+    s2 = Singleton()
+
+    if id(s1) == id(s2):
+        print("Singleton works, both variables contain the same instance.")
+    else:
+        print("Singleton failed, variables contain different instances.")
